a75277fbca94e0bd76135676d00235ed039cbdb4
macosta2015 <macosta2015@my.fit.edu>
2022-11-18 18:59:02 -0500

    Update App.js

--- ninjareact/src/App.js
+++ ninjareact/src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 function App() {
   const title = "Welcome to the new blog"
   const likes=50;
+  const link = "https://github.com"
 
   return (
     <div className="App">
       <div className="content">
         <h1>{ title }</h1>
         <p> Liked {likes} times</p>
+        <p> {Math.random() * 10} </p> 
+
+        <a href={link}> Google Website</a>
+
         <h1>Hello World</h1>
       </div>
     </div>
