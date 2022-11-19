67deeb9cc2e8f27f0346595d451f3a8835d7dea3
macosta2015 <macosta2015@my.fit.edu>
2022-11-19 12:08:54 -0500

    Adding the onClick functions

--- ninjareact/src/Home.js
+++ ninjareact/src/Home.js
@@ -1,15 +1,22 @@
 const Home = () => {
 
     const handleClick = () => {
-        console.log('Hello Ninjas')
+        console.log('Hello Ninjas');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('Hello ' + name);
     }
 
     return (
         <div className="Home">
             <h2>Homepage</h2>
-
             {/* onClick activates the function */}
             <button onClick={handleClick}>Click me</button>
+            <button onClick={() => {
+                handleClickAgain('mario')
+            }}>Click me again</button>
+            {/* <button onClick={handleClickAgain('mario')}>Click me again</button> */}
         </div>
     );
 }
