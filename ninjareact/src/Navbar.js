3fb2a16fdfb836a2217a6b33a4abebf123c049ed
macosta2015 <macosta2015@my.fit.edu>
2022-11-18 21:07:09 -0500

    Adding styles into react

--- ninjareact/src/Navbar.js
+++ ninjareact/src/Navbar.js
@@ -1,9 +1,14 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
+            <h1>Acostas12 Practice</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                    borderRaidus: '8px'
+                }}>New Blog</a>
             </div>
         </nav>
     );
