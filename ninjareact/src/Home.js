0f5a953709f6d2bf83c100f30d15a63767fd828a
macosta2015 <macosta2015@my.fit.edu>
2022-11-19 11:58:32 -0500

    Saturday morning coding

--- ninjareact/src/Home.js
+++ ninjareact/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello Ninjas')
+    }
+
     return (
         <div className="Home">
             <h2>Homepage</h2>
+
+            {/* onClick activates the function */}
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
