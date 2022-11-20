b4a0a3d1ca83708ea3d9bfabb56dd3a5c19e2112
macosta2015 <macosta2015@my.fit.edu>
2022-11-20 10:05:03 -0500

    React blog components

--- ninjareact/src/Home.js
+++ ninjareact/src/Home.js
@@ -1,20 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
-
-    const handleClick = (e) => {
-        console.log('Hello Ninjas', e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('Hello ' + name, e.target);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return (
-        <div className="Home">
-            <h2>Homepage</h2>
-            {/* onClick activates the function */}
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
-            {/* <button onClick={handleClickAgain('mario')}>Click me again</button> */}
+        <div className="home">
+            {blogs.map((blog)=> (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author} </p>
+                </div>
+            ))}
         </div>
     );
 }
