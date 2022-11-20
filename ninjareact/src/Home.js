007d5b93782d87a602179356dd7ba8b9b1f9312a
macosta2015 <macosta2015@my.fit.edu>
2022-11-20 12:43:08 -0500

    Update Home.js

--- ninjareact/src/Home.js
+++ ninjareact/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList'
 
 const Home = () => {
@@ -8,14 +8,24 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name,setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    // Use effect always runs after each render. You can add an argument so that it does not render
+    useEffect(() =>{    
+        console.log('use effect ran')
+        console.log(name);
+    }, [name]); //The name is a dependency, when it runs the function it changes. 
+
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName('Luigi')}>change name</button>
+            <p> {name} </p>
         </div>
     );
 }
