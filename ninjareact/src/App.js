55507c2c56335e41fd108bced056eaa0ed2fd2a4
macosta2015 <macosta2015@my.fit.edu>
2022-11-20 17:15:14 -0500

    Improving the routes.
    It is important to add the 'Switch' and the 'exact'  (<Route exact path="/">).
    
    The switch won't allow the pages and components to render. The exact will help in that as well.

--- ninjareact/src/App.js
+++ ninjareact/src/App.js
@@ -1,22 +1,24 @@
 import Navbar from './Navbar'
 import './index.css'
 import Home from './Home'
+import Create from './Create'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
-  const title = "Welcome to the new blog"
-  const likes=50;
-  const link = "https://github.com"
-
   return (
     <Router>
       <div className="App">
       {/* The Navbar will always show as it is not in the switch statement */}
         <Navbar /> 
         <div className="content">
+          {/* It is always a good idea to sorround the components/routes with the switch components. */}
           <Switch>
-            <Route path="/">
+            {/* You need to add the exact part so that it does not mess uo the routes. */}
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
