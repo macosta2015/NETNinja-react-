b01df649ed1f78e06cb717d0bf75511e944c181d
macosta2015 <macosta2015@my.fit.edu>
2022-11-20 16:27:36 -0500

    Using custom Hooks

--- ninjareact/src/Home.js
+++ ninjareact/src/Home.js
@@ -1,32 +1,8 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    setTimeout(() =>{
-      fetch('http://localhost:8000/blogs')
-        .then(res => {
-          console.log(res)
-          if(!res.ok){
-            throw Error('Could not fetch the data for that resource')
-          }
-          return res.json()
-        })
-        .then(data => {
-          setBlogs(data)
-          setIsPending(false)
-          setError(null)
-        })
-        .catch(err => {
-          setIsPending(false)
-          setError(err.message)
-        })
-    }, 2000)
-  }, []);
+  const {data: blogs, isPending, Error: error} = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
