e7448080e43835e9c8bff08c5d7e09766c7fa215
macosta2015 <macosta2015@my.fit.edu>
2022-11-20 14:07:49 -0500

    Update Home.js

--- ninjareact/src/Home.js
+++ ninjareact/src/Home.js
@@ -6,9 +6,7 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  const [name, setName] = useState('mario');
+  ]);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -16,14 +14,11 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-  }, [name])
+  }, [])
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
     </div>
   );
 }
