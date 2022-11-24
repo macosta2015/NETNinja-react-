10fc1854dcf8271143e47e24583b5cdffa160d96
macosta2015 <macosta2015@my.fit.edu>
2022-11-24 12:54:25 -0500

    Update Create.js

--- ninjareact/src/Create.js
+++ ninjareact/src/Create.js
@@ -3,7 +3,7 @@ const Create = () => {
         <div className="create">
             <h2>Add a New Blog</h2>
 
-            <h2>Discipline is freedom #12 +  </h2>
+            <h2>Discipline is freedom #12 </h2>
         </div>
         );
 }
